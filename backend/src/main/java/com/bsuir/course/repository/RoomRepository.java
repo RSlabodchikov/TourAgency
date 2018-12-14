54a4f905f816891ecf9c91f751c47a973fe5a453
Roman <romanksl34@gmail.com>
2018-12-15 00:04:02 +0300

    +function of creating booking

--- backend/src/main/java/com/bsuir/course/repository/RoomRepository.java
+++ backend/src/main/java/com/bsuir/course/repository/RoomRepository.java
@@ -14,8 +14,8 @@ import java.sql.Date;
 public interface RoomRepository extends CrudRepository<Room, Long> {
     @Query(value = "select *" +
             " from rooms r" +
-            " where r.id not in (select b3.id" +
+            " where r.id not in (select b3.room_id" +
             " from (select * from bookings b1 where (b1.settlement_date <= :inputDate2 AND b1.eviction_date >= :inputDate1)) b3" +
-            " inner join rooms r2 on b3.room_id = r2.id) and r.number_of_clients = :inputNumber", nativeQuery = true)
+            " join rooms r2 on b3.room_id = r2.id) and r.number_of_clients = :inputNumber", nativeQuery = true)
     Iterable<Room> getRoomByDates(@Param("inputDate2") Date settlementDate, @Param("inputDate1") Date evictionDate, @Param("inputNumber") int number);
 }
